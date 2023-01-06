66d7e717e9faff0f425a6a65ea7081b9f0cb9ce3
Jeff Ramos <jeffs.ramos@gmail.com>
2023-01-06 10:38:14 +0000

    build

--- src/scripts/manifests/manifest-json.js
+++ src/scripts/manifests/manifest-json.js
@@ -8,16 +8,16 @@ const assets = [
 	"url":"assets/json\\baseGameConfigFairy.json"
 	},
 	{
-	"id":"baseGameConfigMonster",
-	"url":"assets/json\\baseGameConfigMonster.json"
-	},
-	{
 	"id":"baseGameConfigHumans",
 	"url":"assets/json\\baseGameConfigHumans.json"
 	},
 	{
 	"id":"fairies",
 	"url":"assets/json\\fairies.json"
+	},
+	{
+	"id":"baseGameConfigMonster",
+	"url":"assets/json\\baseGameConfigMonster.json"
 	},
 	{
 	"id":"humans",
@@ -28,16 +28,12 @@ const assets = [
 	"url":"assets/json\\localization_DE.json"
 	},
 	{
-	"id":"localization_EN",
-	"url":"assets/json\\localization_EN.json"
-	},
-	{
 	"id":"localization_ES",
 	"url":"assets/json\\localization_ES.json"
 	},
 	{
-	"id":"localization_FR",
-	"url":"assets/json\\localization_FR.json"
+	"id":"localization_EN",
+	"url":"assets/json\\localization_EN.json"
 	},
 	{
 	"id":"localization_IT",
@@ -48,36 +44,40 @@ const assets = [
 	"url":"assets/json\\localization_JA.json"
 	},
 	{
-	"id":"localization_KO",
-	"url":"assets/json\\localization_KO.json"
+	"id":"localization_FR",
+	"url":"assets/json\\localization_FR.json"
 	},
 	{
-	"id":"localization_PT",
-	"url":"assets/json\\localization_PT.json"
+	"id":"localization_KO",
+	"url":"assets/json\\localization_KO.json"
 	},
 	{
 	"id":"localization_RU",
 	"url":"assets/json\\localization_RU.json"
 	},
 	{
-	"id":"localization_ZH",
-	"url":"assets/json\\localization_ZH.json"
-	},
-	{
 	"id":"localization_TR",
 	"url":"assets/json\\localization_TR.json"
+	},
+	{
+	"id":"localization_ZH",
+	"url":"assets/json\\localization_ZH.json"
 	},
 	{
 	"id":"modifyers",
 	"url":"assets/json\\modifyers.json"
 	},
 	{
+	"id":"monsters",
+	"url":"assets/json\\monsters.json"
+	},
+	{
 	"id":"resources",
 	"url":"assets/json\\resources.json"
 	},
 	{
-	"id":"monsters",
-	"url":"assets/json\\monsters.json"
+	"id":"localization_PT",
+	"url":"assets/json\\localization_PT.json"
 	}
 ];
 
